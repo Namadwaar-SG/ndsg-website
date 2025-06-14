4e37cb59c223f70429a85492728a562c5de41f2e
sssh <1610129@gmail.com>
2025-06-14 15:12:46 +0800

    fix: security

--- app/upload/page.jsx
+++ app/upload/page.jsx
@@ -45,10 +45,13 @@ const Upload = () => {
   const uploadToCloudinary = async (file) => {
     const formData = new FormData();
     formData.append("file", file);
-    formData.append("upload_preset", "NDSG Website");
+    formData.append(
+      "upload_preset",
+      `${process.env.NEXT_PUBLIC_CLOUDINARY_UPLOAD_PRESET}`
+    );
 
     const res = await fetch(
-      "https://api.cloudinary.com/v1_1/dcb3zidq6/image/upload",
+      `https://api.cloudinary.com/v1_1/${process.env.NEXT_PUBLIC_CLOUDINARY_SERVICE_KEY}/image/upload`,
       {
         method: "POST",
         body: formData,
