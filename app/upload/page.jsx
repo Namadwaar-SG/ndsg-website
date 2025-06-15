3175e42118f1fa8ff46a8137e7d88aeebc0fb771
Varun Venkatesh Narayanan <varun.venkatesh-narayanan@keysight.com>
2025-06-15 17:05:13 +0800

    update to docker buildx

--- app/upload/page.jsx
+++ app/upload/page.jsx
@@ -54,10 +54,7 @@ const Upload = () => {
   const uploadToCloudinary = async (file) => {
     const formData = new FormData();
     formData.append("file", file);
-    formData.append(
-      "upload_preset",
-      `${process.env.NEXT_PUBLIC_CLOUDINARY_UPLOAD_PRESET}`
-    );
+    formData.append("upload_preset", "ND SG Website");
 
     const res = await fetch(
       `https://api.cloudinary.com/v1_1/${process.env.NEXT_PUBLIC_CLOUDINARY_SERVICE_KEY}/image/upload`,
