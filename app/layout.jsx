9fb9146a5543726316223ea0b7946cdcea8609d4
Shriniket <24452933+shriniket03@users.noreply.github.com>
2025-08-23 21:21:01 +0800

    implement google analytics

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,23 +1,38 @@
-import React from 'react'
-import '@styles/globals.css'
-import Header from '@app/components/common_components/Header'
-
+import React from "react";
+import "@styles/globals.css";
+import Header from "@app/components/common_components/Header";
+import Script from "next/script";
 
 // common for all pages within app
 export const metadata = {
-    title: "Namadwaar",
-    description:"Official Namadwaar Singapore Website"
-}
+  title: "Namadwaar",
+  description: "Official Namadwaar Singapore Website",
+};
 
-const RootLayout = ({children}) => {
+const RootLayout = ({ children }) => {
   return (
     <html lang="en">
+      <head>
+        {/* <!-- Google tag (gtag.js) --> */}
+        <Script
+          strategy="afterInteractive"
+          src={`https://www.googletagmanager.com/gtag/js?id=${process.env.GOOGLE_ANALYTICS_TAG}`}
+        />
+        <Script id="gtag-init" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${process.env.GOOGLE_ANALYTICS_TAG}');
+          `}
+        </Script>
+      </head>
       <body>
         <Header />
         {children}
       </body>
     </html>
-  )
-}
+  );
+};
 
-export default RootLayout
+export default RootLayout;
